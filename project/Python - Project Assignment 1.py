94b820e31921e9750daebde5934a8f4d84eaf792
Firstname Lastname <harekrishna.mahato2006@gmail.com>
2026-08-30 20:40:12 +0300

    game uploded

---
+++ project/Python - Project Assignment 1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name Gamer: ")
+age = input("Enter your age: ")
+
+print("Welcome to the game", name)
+print("Ah,", age, "years old..")
+print("Let's see if wisdom comes with age...") #(sorry)
---
+++ project/readme.md
@@ -0,0 +1,3 @@
+# The World's Most Serious Game
+
+Harekrishna Mahato
